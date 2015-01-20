243e155a596d426fa4473e203f2301918823c687
John Otander <johnotander@gmail.com>
2015-01-20 14:56:33 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var pseudoClasses = require('..');
+
+describe('pseudo-classes', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(pseudoClasses(), true);
+  });
+});
